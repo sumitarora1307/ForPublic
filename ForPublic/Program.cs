756944076a1176ce10f0cbb73f5bd07610822481
sarora <sarora@Windoows-2016>
2020-04-02 12:24:22 +0000

    Rename to New World

--- ForPublic/Program.cs
+++ ForPublic/Program.cs
@@ -6,7 +6,7 @@ namespace ForPublic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("New World!");
         }
     }
 }
